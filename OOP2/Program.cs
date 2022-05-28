2171f0102f235ca4e4f0d19c92f9e8443ebe7154
AHMET KOCAK <f201213038@ktun.edu.tr>
2022-05-28 13:09:41 +0300

    28.05.2022

---
+++ OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Ahmet";
+            musteri1.SoyAdi = "Kocak";
+            musteri1.TcNo = "123456789";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "9876543210";
+
+            //SOLID
+
+
+            Musteri musteri3 = new GercekMusteri();  // Musteri classı hem GercekMusterinin hem de TuzelMusterinin referansını tutar
+            Musteri musteri4= new TuzelMusteri();
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+            customerManager.Add(musteri3);
+            customerManager.Add(musteri4);
+
+
+        }
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager)
+        {
+            //işlemler
+            krediManager.Hesapla(); 
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediller)
+        {
+            foreach (var kredi in krediller)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
